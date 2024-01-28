42f04a66117b0ee687a06a94e4a18603dc7b3904
Artem Koloskov <artem.koloskov@gmail.com>
2024-01-28 13:04:16 +0700

    Tests: Add unit tests for ResourceManager class

--- ResourceManager.Core.Tests/ResourceManagerTests.cs
+++ ResourceManager.Core.Tests/ResourceManagerTests.cs
@@ -3,8 +3,41 @@ namespace ResourceManager.Core.Tests;
 public class ResourceManagerTests
 {
     [Fact]
-    public void Test1()
+    public void Constructor_WhenJsonFilePathIsInvalid_ThrowsArgumentNullException()
     {
+        // Arrange
+        string? appPath = "";
+        var maxGlobalThreads = 10;
 
+        // Act & Assert
+        Assert.ThrowsAny<Exception>(() => new ResourceManager(appPath, maxGlobalThreads));
+    }
+
+    [Fact]
+    public void Constructor_WhenMaxGlobalThreadsIsLessThanOne_ThrowsArgumentOutOfRangeException()
+    {
+        // Arrange
+        var appPath = "../../../../ConsoleApp/bin/Release/net8.0/ConsoleApp.exe";
+        var maxGlobalThreads = 0;
+        var jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "projects.json");
+
+        // Act & Assert
+        Assert.ThrowsAny<ArgumentOutOfRangeException>(() => new ResourceManager(appPath, maxGlobalThreads));
+    }
+
+    [Fact]
+    public void ExecuteProjectsAsync_ValidJsonFilePath_ExecutesProjects()
+    {
+        // Arrange
+        var appPath = "../../../../ConsoleApp/bin/Release/net8.0/ConsoleApp.exe";
+        var jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "projects.json");
+        var maxGlobalThreads = 10;
+        var resourceManager = new ResourceManager(appPath, maxGlobalThreads);
+
+        // Act
+        resourceManager.ExecuteProjects(jsonFilePath);
+
+        // Assert
+        Assert.True(true);
     }
 }
