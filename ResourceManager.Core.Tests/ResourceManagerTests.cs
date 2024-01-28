4c912f1325564f0faa8c8a7e143de0292a2abf41
Artem Koloskov <artem.koloskov@gmail.com>
2024-01-28 13:52:34 +0700

    Tests: Refactor constructor tests - ctors will accept and validate the json file path too.

--- ResourceManager.Core.Tests/ResourceManagerTests.cs
+++ ResourceManager.Core.Tests/ResourceManagerTests.cs
@@ -3,38 +3,66 @@ namespace ResourceManager.Core.Tests;
 public class ResourceManagerTests
 {
     [Fact]
-    public void Constructor_WhenJsonFilePathIsInvalid_ThrowsArgumentNullException()
+    public void Constructor_AppFilePathIsInvalid_ThrowsArgumentException()
     {
         // Arrange
         string? appPath = "";
+        var jsonFilePath = "projects.json";
         var maxGlobalThreads = 10;
 
         // Act & Assert
-        Assert.ThrowsAny<Exception>(() => new ResourceManager(appPath, maxGlobalThreads));
+        Assert.ThrowsAny<ArgumentException>(() => new ResourceManager(appPath, jsonFilePath, maxGlobalThreads));
     }
 
     [Fact]
-    public void Constructor_WhenMaxGlobalThreadsIsLessThanOne_ThrowsArgumentOutOfRangeException()
+    public void Constructor_AppFilePathIsNotExecutable_ThrowsArgumentException()
+    {
+        // Arrange
+        var appPath = "../../../../ConsoleApp/bin/Release/net8.0/ConsoleApp.dll";
+        var jsonFilePath = "projects.json";
+        var maxGlobalThreads = 10;
+
+        // Act & Assert
+        Assert.ThrowsAny<ArgumentException>(() => new ResourceManager(appPath, jsonFilePath, maxGlobalThreads));
+    }
+
+    [Fact]
+    public void Constructor_JsonFilePathIsInvalid_ThrowsArgumentException()
     {
         // Arrange
         var appPath = "../../../../ConsoleApp/bin/Release/net8.0/ConsoleApp.exe";
+        string? jsonFilePath = "";
+        var maxGlobalThreads = 10;
+
+        // Act & Assert
+        Assert.ThrowsAny<ArgumentException>(() => new ResourceManager(appPath, jsonFilePath, maxGlobalThreads));
+    }
+
+    [Fact]
+    public void Constructor_MaxGlobalThreadsIsLessThanOne_ThrowsArgumentOutOfRangeException()
+    {
+        // Arrange
+        var appPath = "../../../../ConsoleApp/bin/Release/net8.0/ConsoleApp.exe";
+        var jsonFilePath = "projects.json";
         var maxGlobalThreads = 0;
 
         // Act & Assert
-        Assert.ThrowsAny<ArgumentOutOfRangeException>(() => new ResourceManager(appPath, maxGlobalThreads));
+        Assert.ThrowsAny<ArgumentOutOfRangeException>(() => new ResourceManager(appPath, jsonFilePath, maxGlobalThreads));
     }
 
     [Fact]
-    public void ExecuteProjects_WhenJsonFilePathIsInvalid_ThrowsArgumentException()
+    public void Constructor_ValidParameters_DoesNotThrow()
     {
         // Arrange
         var appPath = "../../../../ConsoleApp/bin/Release/net8.0/ConsoleApp.exe";
-        string? jsonFilePath = null;
+        var jsonFilePath = "projects.json";
         var maxGlobalThreads = 10;
-        var resourceManager = new ResourceManager(appPath, maxGlobalThreads);
 
-        // Act & Assert
-        Assert.ThrowsAny<ArgumentException>(() => resourceManager.ExecuteProjects(jsonFilePath));
+        // Act
+        var exception = Record.Exception(() => new ResourceManager(appPath, jsonFilePath, maxGlobalThreads));
+
+        // Assert
+        Assert.Null(exception);
     }
 
     [Fact]
@@ -44,10 +72,10 @@ public class ResourceManagerTests
         var appPath = "../../../../ConsoleApp/bin/Release/net8.0/ConsoleApp.exe";
         var jsonFilePath = "projects.json";
         var maxGlobalThreads = 10;
-        var resourceManager = new ResourceManager(appPath, maxGlobalThreads);
+        var resourceManager = new ResourceManager(appPath, jsonFilePath, maxGlobalThreads);
 
         // Act
-        resourceManager.ExecuteProjects(jsonFilePath);
+        resourceManager.ExecuteProjects();
 
         // Assert
         Assert.True(true);
