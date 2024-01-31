4cbaf5eec74e01d6117779e4875d910c873e2e23
Artem Koloskov <artem.koloskov@gmail.com>
2024-01-31 16:45:35 +0700

    Core: Add `ResourceMonitor` class

---
+++ ResourceManager.Core.Tests/ResourceMonitorTests.cs
@@ -0,0 +1,114 @@
+﻿using System.Runtime.Versioning;
+
+namespace ResourceManager.Core.Tests;
+
+[SupportedOSPlatform("windows")]
+public class ResourceMonitorTests
+{
+    [Fact]
+    public void EnoughResources_EnoughProcessorTimeNotEnoughMemory_ReturnsFalse()
+    {
+        // Arrange
+        _ = ResourceMonitor.TotalProcessingTime();
+        Task.Run(PutLoadOnMemory);
+        Thread.Sleep(3000);
+
+        // Act
+        var enoughResources = ResourceMonitor.EnoughResources(2000);
+
+        // Assert
+        Assert.False(enoughResources);
+    }
+
+    [Fact]
+    public void EnoughResources_EnoughMemoryNotEnoughProcessorTime_ReturnsFalse()
+    {
+        // Arrange
+        _ = ResourceMonitor.TotalProcessingTime();
+        Task.Run(PutLoadOnProcessor);
+        Task.Run(PutLoadOnProcessor);
+        Task.Run(PutLoadOnProcessor);
+        Task.Run(PutLoadOnProcessor);
+        Task.Run(PutLoadOnProcessor);
+        Thread.Sleep(1000);
+
+        // Act
+        var enoughResources = ResourceMonitor.EnoughResources(2000);
+
+        // Assert
+        Assert.False(enoughResources);
+    }
+
+    [Fact]
+    public void EnoughResources_NoLoadOnSystem_ReturnsTrue()
+    {
+        // Arrange
+        Thread.Sleep(1000);
+
+        // Act
+        var enoughResources = ResourceMonitor.EnoughResources(2000);
+
+        // Assert
+        Assert.True(enoughResources);
+    }
+
+    [Fact]
+    public void AverageProcessorTimeInUse_LoadOnSystem_ReturnsValue()
+    {
+        // Arrange
+        _ = ResourceMonitor.TotalProcessingTime();
+        Task.Run(() => PutLoadOnProcessor());
+        Thread.Sleep(1000);
+        _ = ResourceMonitor.EnoughResources(2000);
+
+        // Act
+        var averageProcessorTimeInUse = ResourceMonitor.AverageProcessorTimeInUse();
+
+        // Assert
+        Assert.True(averageProcessorTimeInUse > 0);
+    }
+    
+    [Fact]
+    public void TotalProcessingTime_LoadOnSystem_ReturnsValue()
+    {
+        // Arrange
+        _ = ResourceMonitor.TotalProcessingTime();
+        Task.Run(() => PutLoadOnProcessor());
+        Thread.Sleep(1000);
+
+        // Act
+        var totalProcessingTime = ResourceMonitor.TotalProcessingTime();
+
+        // Assert
+        Assert.True(totalProcessingTime > 1000, $"Total processing time is {totalProcessingTime} ms.");
+    }
+
+    private static void PutLoadOnProcessor()
+    {
+        var rnd = new Random();
+        float result = 0;
+        while(true)
+        {
+            result += rnd.Next(0, 1000) / rnd.Next(1, 1000) / rnd.Next(1, 1000);
+        }
+    }
+
+    private static void PutLoadOnMemory()
+    {
+        var rnd = new Random();
+        var list = new List<byte[]>();
+        
+        for (int i = 0; i < 10; i++)
+        {
+            var array = new byte[1024 * 1024 * 2000];
+            list.Add(array);
+        }
+
+        while(true)
+        {
+            var index = rnd.Next(0, list.Count);
+            var index2 = rnd.Next(0, 1024 * 1024 * 2000);
+            list[index][index2] = 1;
+        }
+    }
+}
---
+++ ResourceManager.Core/ResourceMonitor.cs
@@ -0,0 +1,133 @@
+﻿using System.Diagnostics;
+using System.Runtime.Versioning;
+
+namespace ResourceManager.Core;
+
+[SupportedOSPlatform("windows")]
+public class ResourceMonitor
+{
+    private const int DELAY_BETWEEN_CHECKS = 50;
+    private const float MIN_PROCESSOR_TIME_THRESHOLD = 30f;
+    private const int MIN_MEMORY_THRESHOLD_BYTES = 1024 * 1024 * 200;
+    private static readonly PerformanceCounter _availableMemoryBytes =
+        new("Memory", "Available Bytes");
+    private static readonly PerformanceCounter _processorTime =
+        new("Processor", "% Processor Time", "_Total");
+
+    private static readonly Stopwatch _stopwatch = new();
+    private static readonly Logger<ResourceMonitor> _log = new("log")
+    {
+#if DEBUG
+        IsEnabled = true
+#else
+        IsEnabled = false
+#endif
+    };
+
+    private static int _memoryThresholdBytes = MIN_MEMORY_THRESHOLD_BYTES;
+    private static float _processorTimeThreshold = MIN_PROCESSOR_TIME_THRESHOLD;
+    private static int _processorTimeCallsCount = 0;
+    private static float _processorTimeSum = 0;
+
+
+    static ResourceMonitor()
+    {
+        _stopwatch.Start();
+        _processorTime.NextValue();
+
+        _log.Log("ResourceMonitor initialized.");
+    }
+
+    public static void Configure(int memoryThresholdBytes, float processorTimeThreshold)
+    {
+        _log.Log($"Configuring ResourceMonitor with " +
+            $"memoryThresholdBytes: {memoryThresholdBytes} and " +
+            $"processorTimeThreshold: {processorTimeThreshold}");
+
+        ArgumentOutOfRangeException.ThrowIfLessThan(memoryThresholdBytes, MIN_MEMORY_THRESHOLD_BYTES);
+        ArgumentOutOfRangeException.ThrowIfLessThan(processorTimeThreshold, MIN_PROCESSOR_TIME_THRESHOLD);
+
+        _memoryThresholdBytes = memoryThresholdBytes;
+        _processorTimeThreshold = processorTimeThreshold;
+    }
+
+    public static void WaitForEnoughResources(int memoryToAllocateMb)
+    {
+        _log.Log($"Waiting for enough resources to allocate " +
+            $"{memoryToAllocateMb} MB.");
+
+        while (!EnoughResources(memoryToAllocateMb))
+        {
+            Thread.Sleep(DELAY_BETWEEN_CHECKS);
+        }
+
+        _log.Log($"Enough resources to allocate " +
+            $"{memoryToAllocateMb} MB.");
+    }
+
+    public static bool EnoughResources(int memoryToAllocateMb)
+    {
+        return EnoughProcessorTime() && EnoughMemory(memoryToAllocateMb);
+    }
+
+    public static float AverageProcessorTimeInUse()
+    {
+        if (_processorTimeCallsCount == 0)
+        {
+            _log.Log("_processorTimeCallsCount is 0.");
+
+            return 0;
+        }
+
+        var result = _processorTimeSum / _processorTimeCallsCount;
+
+        _log.Log($"Average processor time in use: {result}%.");
+
+        return result;
+    }
+
+    public static long TotalProcessingTime()
+    {
+        var result = _stopwatch.ElapsedMilliseconds;
+
+        _log.Log($"Total processing time: {result} ms.");
+
+        return result;
+    }
+
+    private static bool EnoughProcessorTime()
+    {
+        var availableProcessorTime = 100 - GetProcessorTime();
+
+        _log.Log($"Available processor time: {availableProcessorTime}%. " +
+            $"Processor time threshold: {_processorTimeThreshold}%.");
+
+        var result = availableProcessorTime > _processorTimeThreshold;
+
+        _log.Log($"Enough processor time: {result}.");
+
+        return result;
+    }
+
+    private static bool EnoughMemory(int memoryToAllocateMb)
+    {
+        var availableMemoryMb = _availableMemoryBytes.RawValue / 1024 / 1024;
+        var memoryThresholdMb = _memoryThresholdBytes / 1024 / 1024;
+
+        _log.Log($"Available memory: {availableMemoryMb} MB. " +
+            $"Memory threshold: {memoryThresholdMb} MB. " +
+            $"Memory to allocate: {memoryToAllocateMb} MB.");
+
+        return availableMemoryMb > memoryThresholdMb + memoryToAllocateMb;
+    }
+
+    private static float GetProcessorTime()
+    {
+        float processorTime = _processorTime.NextValue();
+        _processorTimeCallsCount++;
+
+        _processorTimeSum += processorTime;
+
+        return processorTime;
+    }
+}
